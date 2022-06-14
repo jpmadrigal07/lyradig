56945f7770aaa488ecd5ed81eb956a399be0a621
John Patrick Madrigal <jp.madrigal07@gmail.com>
2022-06-14 09:03:29 +0800

    refactor: added button sample for styling and remove hello.js

---
+++ styles/button.js
@@ -0,0 +1,19 @@
+const regular = (addedClass) => {
+
+}
+
+const success = (addedClass) => {
+
+}
+
+const danger = (addedClass) => {
+
+}
+
+const info = (addedClass) => {
+
+}
+
+const custom = (addedClass) => {
+
+}
